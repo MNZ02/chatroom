3a9d90837f7b7aa249a1ef0a9b964b335aa3d986
mnz02 <abdulminhaz47@gmail.com>
2025-01-07 19:45:11 +0530

    Add getCurrentUser endpoint and refactor getAllUsers logic

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,14 +4,13 @@ import User from "../models/User";
 export async function getAllUsers(req: Request, res: Response) {
   try {
     const currentUser = req.user;
-    console.log({ currentUser });
 
     if (!currentUser) {
       res.status(404).json({ message: "User not authenticated" });
     }
     const users = await User.find({
       username: {
-        $ne: currentUser ,
+        $ne: currentUser,
       },
     });
     if (users.length == 0) {
@@ -21,6 +20,21 @@ export async function getAllUsers(req: Request, res: Response) {
     res.status(200).json({ users });
   } catch (error) {
     console.error("Error", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function getCurrentUser(req: Request, res: Response) {
+  try {
+    const user = req.user;
+    const currentUser = await User.findOne({ username: user });
+    if (!currentUser) {
+      res.status(404).json({ message: "User not logged in or not found" });
+      return;
+    }
+    res.status(200).json({ currentUser });
+  } catch (error) {
+    console.error("Error getting current user", error);
     res.status(500).json({ message: "Internal server error" });
   }
 }
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,8 +1,9 @@
-import { getAllUsers } from "../controllers/userController";
+import { getAllUsers, getCurrentUser } from "../controllers/userController";
 import { authMiddleware } from "../middleware/authMiddleware";
 import express from "express";
 const router = express.Router();
 
 router.get("/users", authMiddleware, getAllUsers);
+router.get("/user", authMiddleware, getCurrentUser);
 
 export default router;
